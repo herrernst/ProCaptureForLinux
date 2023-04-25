a5eb335c4517e3a7ffea9c9d02d75aaa90d052f5
herrernst <herr.ernst@gmail.com>
2023-04-25 22:14:47 +0200

    initial import
    https://www.magewell.com/files/drivers/ProCaptureForLinux_4236.tar.gz
    
    e4683c3868608869395eb7667adfbe987971f10ede36140bd6e6883a0992cfd5

---
+++ src/public/ProductVer.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define VER_MAJOR						1
+#define VER_MINOR						3
+#define VER_RELEASE						0
+#define VER_BUILD						4236
+#define VER_DATE						"2021-03-01 15:40:48"
+#define VER_CLASS		  "RC"
